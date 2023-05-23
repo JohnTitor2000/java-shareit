e635a45cd58cd321004894252e7f4dd4355f2b2e
Vitalii Uriku <johntitor2000@yandex.ru>
2023-05-23 17:43:37 +0300

    refactor 2

--- src/main/java/ru/practicum/shareit/item/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/CommentMapper.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.exaption.NotFoundException;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
-import ru.practicum.shareit.user.UserRepository;
 
 import java.time.LocalDateTime;
 
--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoDefault.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoDefault.java
@@ -1,10 +1,7 @@
 package ru.practicum.shareit.item.dto;
-
 import lombok.Data;
-import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
-import javax.persistence.*;
 @Data
 public class ItemDtoDefault {
     private Long id;
